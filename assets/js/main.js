8a15374842c593effa078fbaedcf31b8498b7735
Alish Hajiyev <alish.haciyev@mail.ru>
2022-11-18 22:02:06 +0400

    Final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,76 @@
-let body = document.body;
-let currencyRow1 = document.querySelector('#currency-row1');
-let currencyRow2 = document.querySelector('#currency-row2');
+let list = document.querySelector(".list");
+let media = document.querySelector(".media");
+let currencyRow = document.querySelector('#container');
 let p1= document.querySelector('.area1>p');
 let p2= document.querySelector('.area2>p');
-let input1 = document.querySelector('#input1')
+let input1 = document.querySelector('#input1');
 let input2 = document.querySelector('#input2')
+let one = document.getElementsByName("one");
+let two = document.getElementsByName("two");
 
-let state1= 'RUB';
-let state2= 'USD';
-// If clicked state
-currencyRow1.addEventListener("click", e=>{
-    if(e.target.className==='currency1'){
-        let arr = [];
-        for( let elem of e.target.parentElement.children){
-            arr.push(elem)
-        }
-        arr.forEach((item)=>{
-            if(item===e.target){
-                console.log(item, e.target)
-                e.target.classList.add('violet')
-                console.log(e.target.classList)
-            }
-            if(item!==e.target){
-                item.classList.remove('violet')
-            }
+let first;
+let second;
+// Changing of color
+currencyRow.addEventListener('click', e=>{
+    if(e.target.classList.contains('currency1')) func(e);
+    else if(e.target.classList.contains('currency2')) func(e);
+})
+// Functions
+function func(e){
+    let arr = [...e.target.parentElement.children];
+    arr.forEach(item => {
+        if (item.classList.contains("active")) {
+            item.classList.remove("active");
+        };
+    });
+    e.target.classList.add("active");
+}
+
+// Calculation
+function calc(){
+    one.forEach(item => {
+        if (item.classList.contains("active")) {
+            first = item.innerText;
+        };
     })
+    two.forEach(item => {
+        if (item.classList.contains("active")) {
+            second = item.innerText;
+        };
+    })
+    fetch(`https://api.exchangerate.host/latest?base=${first}&symbols=${second}`)
+        .then(r => r.json())
+        .then((data) => {
+            input2.value = Number(input1.value) * data.rates[second];
+            p1.innerText = `1 ${first} = ${data.rates[second]} ${second}`;
+            p2.innerText = `1 ${second} = ${1 / data.rates[second]} ${first}`;
+        }).catch(error => {alert("Check your internet connection");console.log("Check your internet connection")})
+}
+// Calculation2
+function calc2(){
+    one.forEach(item => {
+        if (item.classList.contains("active")) {
+            first = item.innerText;
+        }
+    })
+    two.forEach(item => {
+        if (item.classList.contains("active")) {
+            second = item.innerText;
+        };
+    })
+    fetch(`https://api.exchangerate.host/latest?base=${second}&symbols=${first}`)
+        .then(r => r.json())
+        .then((data) => {
+            input1.value = Number(input2.value) * data.rates[first];
+            p2.innerText = `1 ${second} = ${1 / data.rates[first]} ${first}`;
+            p1.innerText = `1 ${first} = ${data.rates[first]} ${second}`;
+        }).catch(error => {alert("Check your internet connection");console.log("Check your internet connection")})
+}
 
-        fetch(`https://api.exchangerate.host/latest?base=${e.target.innerText}&symbols=${state2}`)
-        .then(r=>r.json())
-        .then(data=>{
-            state1=e.target.innerText;
-            p1.innerText=`1 ${state1} = ${(+Object.values(data.rates)).toFixed(3)} ${state2}`;
-            p2.innerText=`1 ${state2} = ${(1/Object.values(data.rates)).toFixed(3)} ${state1}`;
+input1.addEventListener('keypress', calc);
+input2.addEventListener('keypress', calc2);
 
-            if(input2.value==='' || typeof input2.value==='string'){
-                input2.value=(input1.value*Object.values(data.rates)).toFixed(3);
-            }
-            else if(input1.value===''){
-                input1.value=(input2.value/Object.values(data.rates)).toFixed(3);
-            }
-        })
-    }
-})
-
-currencyRow2.addEventListener("click", e=>{
-    if(e.target.className==='currency2'){
-
-
-        fetch(`https://api.exchangerate.host/latest?base=${e.target.innerText}&symbols=${state2}`)
-        .then(r=>r.json())
-        .then(data=>{
-            state2=e.target.innerText;
-            p1.innerText=`1 ${state1} = ${(+Object.values(data.rates)).toFixed(3)} ${state2}`;
-            p2.innerText=`1 ${state2} = ${(1/Object.values(data.rates)).toFixed(3)} ${state1}`;
-
-            if(input2.value==='' || typeof input2.value==='string'){
-                input2.value=(input1.value*Object.values(data.rates)).toFixed(3);
-            }
-            else if(input1.value===''){
-                input1.value=(input2.value/Object.values(data.rates)).toFixed(3);
-            }
-        })
-    }
-})
+function navBar(){
+    list.classList.toggle("show");
+}
+media.addEventListener("click", navBar);
